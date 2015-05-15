83fec36ce4d8e06e162da46e69248e518e3c64ad
anjarganteng <ganjar.snjy@gmail.com>
2015-05-16 01:49:37 +0700

    ExportPDF name .php

--- application/bdi/component/cetak_umat_almt/cetak_umat_almt.php
+++ application/bdi/component/cetak_umat_almt/cetak_umat_almt.php
@@ -51,7 +51,7 @@ include "../../function/functionaction.php";
 ?>
 <?php
 
-$exportpdf = "exportPdf('pdf','pdf-country','');"; //TYPE EXPORT, FILE NAME EXPORT, PARAMETER ex  : 'pdf','pdf-country','&id=id'
+$exportpdf = "exportPdf('pdf','pdf-umat-by-alamat','');"; //TYPE EXPORT, FILE NAME EXPORT, PARAMETER ex  : 'pdf','pdf-country','&id=id'
 $exportexcel = "exportExcel('excel','excel-country','');";
 
 include "../../function/contentmodul.html.php";
